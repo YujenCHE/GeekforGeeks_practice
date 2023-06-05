b849885933d17e78e919efbbd1537b3e14c3d21a
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-05 17:15:45 +0800

    Create How to Take String Input_Using getline .cpp

---
+++ How to Take String Input_Using getline .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //用getline
+    string d;
+    cout<<"Enter string"<<endl;
+    getline(cin,d);
+    cout<<"Your 2nd string is "<<d<<endl;
+
+    return 0;
+}
+
+
