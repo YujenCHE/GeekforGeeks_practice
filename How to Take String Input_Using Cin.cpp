1df3b9cfbafd3e099e930ee4754720374db604ea
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-05 17:14:10 +0800

    Create How to Take String Input_Using Cin.cpp

---
+++ How to Take String Input_Using Cin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    string s;
+    
+    //用cin
+    cout<<"Enter string"<<endl;
+    cin>>s;
+    cout<<"Your 1st string is "<<s<<endl;
+    
+    
+    
+    return 0;
+}
