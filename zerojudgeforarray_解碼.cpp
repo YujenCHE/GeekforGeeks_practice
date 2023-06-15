9a70e5849c175749217b3f7cfd9e9460dc508c7d
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-15 09:29:30 +0800

    Create zerojudgeforarray_解碼.cpp
    unfinish

---
+++ zerojudgeforarray_解碼.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m,n,i;
+    string s,e;
+    cin>>m>>n;
+    
+    for(i=1;i<=m;i++)
+    {
+         for(i=e.size()-1;i<=n;i++)
+         {
+            
+         }
+    }
+}
