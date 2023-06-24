c1e67e74c68d25f3c05ebb6aea7463c81b615f9a
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-24 15:04:56 +0800

    Update array and pointers are different.cpp

--- array and pointers are different.cpp
+++ array and pointers are different.cpp
@@ -8,9 +8,11 @@ int main()
     int* ptr = arr;
     
     // sizof(int) * (number of element in arr[]) is printed ==> ??
+    //陣列的大小 = 4 bytes * 5個元素 
     cout << "The size of array is " << sizeof(arr)<<endl;
     
-    // sizeof a pointer is printed which is same for all ==>??
+    // sizeof a pointer is printed which is same for all
+    // ==> GDT "指標的大小在大多數系統中是固定的，通常為 4 或 8 個位元組，不論指標所指向的型別是什麼。"
     // type of pointers (char *, void *, etc)    
     cout << "However, the size of pointer is " << sizeof(ptr) << endl;
     
